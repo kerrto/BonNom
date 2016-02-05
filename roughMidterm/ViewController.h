8b502cf8adc3e119fc3d5bc7c5dd7531e13b8188
Kerry Toonen <kerrytoonen@gmail.com>
2016-02-04 23:15:46 -0500

    Initial Commit

---
+++ roughMidterm/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  roughMidterm
+//
+//  Created by Kerry Toonen on 2016-02-04.
+//  Copyright © 2016 Kerry Toonen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
